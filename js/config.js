0c5a255963063b03184876882067e2f8c19654ee
vesasc1962 <vesasc.1962@gmail.com>
2026-02-10 17:18:12 +0530

    Update Cloudflare tunnel URL and fix YouTube video playback

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Configuration
 const config = {
     // Backend server URL - UPDATE THIS when you get a new tunnel URL
-    SERVER_URL: 'https://differential-blvd-mrs-particle.trycloudflare.com'
+    SERVER_URL: 'https://luther-tells-object-palace.trycloudflare.com'
 };
