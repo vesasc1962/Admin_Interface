53604602fb050e6f0fddaa5ffa076ceb13a3fe78
vesasc1962 <vesasc.1962@gmail.com>
2026-02-11 23:16:39 +0530

    Update Cloudflare tunnel URL to medium-chicago-revenues-sue

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Configuration
 const config = {
     // Backend server URL - UPDATE THIS when you get a new tunnel URL
-    SERVER_URL: 'https://luther-tells-object-palace.trycloudflare.com'
+    SERVER_URL: 'https://medium-chicago-revenues-sue.trycloudflare.com'
 };
